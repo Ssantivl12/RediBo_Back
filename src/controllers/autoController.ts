cb6b9fdd3d9319a270a1e59b9d26ab0b97fb1065
AngelTola <tolaangel606@gmail.com>
2025-04-12 01:15:33 -0400

    feat(prisma/api): modelo host, api para obtener el host asociado a un auto

--- src/controllers/autoController.ts
+++ src/controllers/autoController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { stripTypeScriptTypes } from "module";
 
 const prisma = new PrismaClient();
 
@@ -136,6 +137,46 @@ export const getComentarios = async (req: Request, res: Response): Promise<void>
       res.status(500).json({
         success: false,
         message: "Error al obtener las calificaciones del auto.",
+        error: error instanceof Error ? error.message : "Error desconocido",
+      });
+    }
+  };
+
+  export const getHost = async (req: Request, res: Response): Promise<void> => {
+    const id = parseInt(req.params.id, 10);
+  
+    if (isNaN(id)) {
+      res.status(400).json({
+        success: false,
+        message: "ID inválido proporcionado.",
+      });
+      return;
+    }
+  
+    try {
+      const autoHost = await prisma.auto.findUnique({
+        where: {
+          idAuto: id},
+          include: { host: true},
+      });
+  
+      if (!autoHost || !autoHost.host) {
+        res.status(404).json({
+          success: false,
+          message: "Host no encontrado.",
+        });
+        return;
+      }
+  
+      res.status(200).json({
+        success: true,
+        data: autoHost.host, 
+      });
+  
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error al obtener el HOST del auto.",
         error: error instanceof Error ? error.message : "Error desconocido",
       });
     }
--- src/routes/autoRoutes.ts
+++ src/routes/autoRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAutos, getAutoId, getComentarios, getCalificacion} from "../controllers/autoController";
+import { getAutos, getAutoId, getComentarios, getCalificacion, getHost} from "../controllers/autoController";
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.get('/autos', getAutos);
 router.get('/autos/:id', getAutoId);
 router.get('/autos/:id/comentarios', getComentarios);
 router.get('/autos/:id/calificacion', getCalificacion);
+router.get('/autos/:id/host', getHost);
 
 export default router;
