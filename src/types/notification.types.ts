5137d3e0ec3c51274d3a29b0516dd00d0010cd44
ssantivl <santi.salguerolopez@gmail.com>
2025-04-14 19:15:52 -0400

    feat: types de notificaciones agregados

---
+++ src/types/notification.types.ts
@@ -0,0 +1,31 @@
+import { TipoDeNotificacion, PrioridadNotificacion } from "@prisma/client";
+
+// Data Transfer Object, estructura exacta para crear una nueva notificacion en el sistema
+export interface NotificacionDTO{
+    titulo: string;
+    mensaje: string;
+    tipo: TipoDeNotificacion;
+    prioridad?: PrioridadNotificacion;
+    entidadId?: string;
+    tipoEntidad?: string;
+    usuarioId: string;
+}
+
+// Campos opcionales para los criterios por los que pueden ser filtradas las notificaciones
+export interface NotificacionFiltro{
+    usuarioId?: string;
+    tipo?: TipoDeNotificacion;
+    leido?: boolean;
+    prioridad?: PrioridadNotificacion;
+    desde?: Date;
+    hasta?: Date;
+    tipoEntidad?: string;
+    limit?: number;
+    offset?: number;
+}
+
+export interface NotificacionWebSocket{
+    evento: 'NUEVA_NOTIFICACION' | 'NOTIFICACION_LEIDA' | 'NOTIFICACION_ELIMINADA';
+    data: any;
+    usuarioId: string;
+}
