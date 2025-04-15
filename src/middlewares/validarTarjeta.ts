da2b131a98cbbc19d8534729c9f28b0805381491
elCheetah <eabraham.pro@gmail.com>
2025-04-15 09:00:19 -0400

    validacion de tarjeta de pago

---
+++ src/middlewares/validarTarjeta.ts
@@ -0,0 +1,37 @@
+export function validarTarjeta(
+    nombreTitular,
+    numeroTarjeta,
+    fechaExpiracion,
+    cvv,
+    direccion,
+    correoElectronico
+) {
+    const errores = [];
+
+    const nombreValido = /^[A-ZÁÉÍÓÚÑ][A-ZÁÉÍÓÚÑa-záéíóúñ]+\s[A-ZÁÉÍÓÚÑa-záéíóúñ\s]+$/.test(nombreTitular.trim());
+    if (!nombreValido) errores.push("Nombre del titular inválido");
+
+    if (!/^\d{16}$/.test(numeroTarjeta)) errores.push("Número de tarjeta inválido");
+
+    const fechaRegex = /^(0[1-9]|1[0-2])\/\d{2}$/;
+    if (!fechaRegex.test(fechaExpiracion)) {
+        errores.push("Formato de fecha de expiración inválido");
+    } else {
+        const [mes, anio] = fechaExpiracion.split('/');
+        const fechaActual = new Date();
+        const anioCompleto = 2000 + parseInt(anio);
+        const fechaIngresada = new Date(anioCompleto, parseInt(mes), 0);
+        if (fechaIngresada < fechaActual) errores.push("La tarjeta está vencida");
+    }
+
+    if (!/^\d{3}$/.test(cvv)) errores.push("CVV inválido");
+
+    if (!direccion || direccion.trim().length < 5) errores.push("Dirección inválida");
+
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correoElectronico);
+    if (!emailValido) errores.push("Correo electrónico inválido");
+
+    return errores.length > 0
+        ? { valido: false, errores }
+        : { valido: true };
+}
