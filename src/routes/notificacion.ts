71e6c7cef3fb894fc001a6acf7b12167fe76ac22
ssantivl <santi.salguerolopez@gmail.com>
2025-04-16 21:06:17 -0400

    feat: rutas agregadas

---
+++ src/routes/notificacion.ts
@@ -0,0 +1,46 @@
+// routes/notificacion.ts
+import { Router } from 'express';
+import { NotificacionController } from '../controllers/notificacion';
+import { autenticarJWT } from '../middlewares/autenticacion';
+
+// Función que crea las rutas y recibe el controlador como parámetro
+export const createNotificacionRoutes = (notificacionController: NotificacionController) => {
+  const router = Router();
+
+  // panel de notificaciones
+  router.get(
+    '/panel/:usuarioId',
+    autenticarJWT,
+    (req, res) => notificacionController.obtenerPanelNotificaciones(req, res)
+  );
+
+  // eliminar notificación
+  router.delete(
+    '/:id',
+    autenticarJWT,
+    (req, res) => notificacionController.eliminarNotificacion(req, res)
+  );
+
+  // detalle de una notificación
+  router.get(
+    '/detalle/:id',
+    autenticarJWT,
+    (req, res) => notificacionController.obtenerDetalleNotificacion(req, res)
+  );
+
+  // marcar notificación como leída
+  router.put(
+    '/marcar-leida/:id',
+    autenticarJWT,
+    (req, res) => notificacionController.marcarComoLeida(req, res)
+  );
+
+  //obtener conteo de notificaciones no leidas
+  router.get(
+    '/no-leidas/:usuarioId',
+    autenticarJWT,
+    (req, res) => notificacionController.obtenerConteoNoLeidas(req, res)
+  );
+
+  return router;
+};
