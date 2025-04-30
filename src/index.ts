7205546a64b2d686ca7a2ebfe4e628dd4f85e024
Cristhian_Lopez_Ayne <cristhianlopezayne51@gmail.com>
2025-04-30 18:24:40 -0400

    configure el servidor

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,57 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import cors from 'cors';
+
 import rutasPago from './routes/pago.routes';
 import qrRoutes from './routes/generarQRRoute';
 import historialBusquedaRoutes from './routes/historialBusquedaRoutes';
-import  vehiculoRoutes  from './routes/vehiculoRoutes';
-import reservasRoutes from './routes/reservas.routes'
+import vehiculoRoutes from './routes/vehiculoRoutes';
+import reservasRoutes from './routes/reservas.routes';
 import mapaRoutes from './routes/filtroMapaPrecioRoutes';
-
-
-const cors = require('cors');
-
+//import vehiculosRoutes from './routes/vehiculos.routes'; // la nueva ruta para GPS
 
 const app = express();
 dotenv.config();
+
 app.use(cors());
 app.use(express.json());
-
 
 // Rutas de APIs
 app.use('/pagos', rutasPago);
 app.use('/', qrRoutes);
 app.use('/historial', historialBusquedaRoutes);
 app.use('/vehiculo', vehiculoRoutes);
-app.use('/reservas',reservasRoutes);
-app.use('/mapa',mapaRoutes);
-
-
+app.use('/reservas', reservasRoutes);
+app.use('/mapa', mapaRoutes);
+//app.use('/vehiculos', vehiculosRoutes); // Ruta para el filtrado por GPS
 
 // Ruta pública para comprobantes (se sirve desde public/cmp)
-app.use('/cmp', express.static(path.join(process.cwd(), 'public', 'cmp'), {
-  setHeaders: (res, path) => {
-    if (path.endsWith('.png')) {
-      res.set('Content-Type', 'image/png');
-    }
-  }
-}));
+app.use(
+  '/cmp',
+  express.static(path.join(process.cwd(), 'public', 'cmp'), {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.png')) {
+        res.set('Content-Type', 'image/png');
+      }
+    },
+  })
+);
 
 // Ruta pública para archivos QR (se sirve desde public/qr)
-app.use('/qr', express.static(path.join(process.cwd(), 'public', 'qr'), {
-  setHeaders: (res, path) => {
-    if (path.endsWith('.png')) {
-      res.set('Content-Type', 'image/png');
-    }
-  }
-}));
+app.use(
+  '/qr',
+  express.static(path.join(process.cwd(), 'public', 'qr'), {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.png')) {
+        res.set('Content-Type', 'image/png');
+      }
+    },
+  })
+);
 
-
-
-
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
-});
-//Ruta para historia de gps 
-import express from 'express';
-import vehiculosRoutes from './routes/vehiculos.routes';
-
-const app = express();
+// Iniciar servidor
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-// Usar la ruta de vehículos
-app.use('/vehiculos', vehiculosRoutes);
-
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
