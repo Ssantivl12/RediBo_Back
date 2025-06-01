e64d848e56b3a9427fdd46f2ce9516a291dd9673
Melina Montaño <202200060@est.umss.edu>
2025-05-31 20:05:55 -0400

    correccion de ruta

--- src/routes/auth/listaDrivers.routes.ts
+++ src/routes/auth/listaDrivers.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import { isAuthenticated } from '../../middlewares/auth/isAuthenticated';
-import { registrarDriverController } from '../../controllers/auth/authRegistroDriver/registroDriver.controller';
+import { getDriversByRenter } from '../../controllers/auth/listaDrivers/listaDrivers.controller';
+import { requireAuth } from '../../middlewares/auth/authMiddleware';
 
+export const router = Router();
 
-export const registroDriverRouter = () => {
-  const router = Router();
-  router.post('/registro-driver', isAuthenticated, registrarDriverController);
-  return router;
-};
-
+router.get('/drivers-by-renter', requireAuth, (req, res, next) => {
+  Promise.resolve(getDriversByRenter(req, res)).catch(next);
+});
