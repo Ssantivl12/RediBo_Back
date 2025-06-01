db0cb14aeb8f02c4cd585cfeb8aa21577bf7dab2
Melina Montaño <202200060@est.umss.edu>
2025-05-31 20:07:55 -0400

    Correcion de la ruta en produccion

--- src/routes/auth/registroDriver.routes.ts
+++ src/routes/auth/registroDriver.routes.ts
@@ -2,8 +2,6 @@ import { Router } from 'express';
 import { registrarDriverController } from '../../controllers/auth/authRegistroDriver/registroDriver.controller';
 import { isAuthenticated } from "../../middlewares/auth/isAuthenticated";
 
-const router = Router();
-
+export const router = Router();
 router.post('/registro-driver', isAuthenticated, registrarDriverController);
 
-export default router;
