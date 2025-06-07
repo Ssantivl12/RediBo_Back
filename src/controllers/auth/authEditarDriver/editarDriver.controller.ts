c41b2c59642e41bfa3d9394edf762b847585cda0
Alan <ferchoalan81@gmail.com>
2025-06-07 15:00:26 -0400

    Implemetar el request en el user y files del driver

--- src/controllers/auth/authEditarDriver/editarDriver.controller.ts
+++ src/controllers/auth/authEditarDriver/editarDriver.controller.ts
@@ -1,2 +1,14 @@
 import {request, response} from 'express';
 import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// Extendemos Request para incluir `user` y `files` con tipado más claro
+interface MulterRequest extends Request {
+  user?: {
+    idUsuario: number;
+  };
+  files?: {
+    [fieldname: string]: Express.Multer.File[];
+  };
+}
