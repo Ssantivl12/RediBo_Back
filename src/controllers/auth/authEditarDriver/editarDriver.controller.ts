6b959f25595f22a075674461a79416fad5426ce0
Alan <ferchoalan81@gmail.com>
2025-06-07 15:05:16 -0400

    implementacion del codigo para extraer datos del driver

--- src/controllers/auth/authEditarDriver/editarDriver.controller.ts
+++ src/controllers/auth/authEditarDriver/editarDriver.controller.ts
@@ -1,4 +1,4 @@
-import {request, response} from 'express';
+import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -11,4 +11,29 @@ interface MulterRequest extends Request {
   files?: {
     [fieldname: string]: Express.Multer.File[];
   };
+}
+
+export const editarPerfilDriver = async (req: MulterRequest, res: Response): Promise<void> => {
+  const idUsuario = req.user?.idUsuario;
+
+  if (!idUsuario) {
+    res.status(401).json({ message: "No autorizado: token inválido" });
+    return;
+  }
+
+  try {
+    // Extraemos campos del body
+    const {
+      telefono,
+      licencia,
+      tipoLicencia,
+      fechaEmision,
+      fechaExpiracion
+    } = req.body;
+
+    // Extraemos archivos subidos (si existen)
+    const anversoFile = req.files?.["anverso"]?.[0];
+    const reversoFile = req.files?.["reverso"]?.[0];
+
+    
 }
