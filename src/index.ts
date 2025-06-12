d29b54b42c302e81f1b4160a21d921f98dbc8051
JhairCJ <jhaircjxd@gmail.com>
2025-06-11 20:49:48 -0400

    fix: Integracion del backend de CodeLovers

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import usuarioRoutes from "./routes/auth/usuario.routes";
 //import visualizarDriverRoutes from "./routes/auth/visualizarDriver.routes";
 import listaDriversRoutes from "./routes/auth/listaDrivers.routes";
 import visualizarRentersRoutes from "./routes/auth/visualizarRenters.routes";
-
+import routesCodeLovers from "./routes/routesCodelovers"
 // Verificación en 2 pasos
 import twofaRoutes from "./routes/auth/twofa.routes";
 
@@ -153,6 +153,8 @@ app.use("/api", visualizarRentersRoutes);
 app.use("/api", listaDriversRoutes);
 app.use("/api", twofaRoutes);
 
+
+app.use('/api', routesCodeLovers);
 // Rutas de api - SpeedCode
 app.use('/api', mapaRoutes);
 
